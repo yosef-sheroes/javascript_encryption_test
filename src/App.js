ad8c0066c4441078fcef246b67449251345e44d3
Yosef <yosef@sheroes.in>
2022-12-13 12:03:15 +0300

    update app.js and data.js refactor and optimization

--- src/App.js
+++ src/App.js
@@ -1,125 +1,47 @@
 import JsEncrypt from 'jsencrypt';
 import raw from '../src/hybrid-public.pem';
 import publickey from './thirdpartypublic'
-import { useEffect } from 'react';
+import data, { request } from './data';
+import { useEffect,useState } from 'react';
 
+const styles = {
+
+  button_style:{
+  width:250,
+  height:50,
+  backgroundColor:"#DC4441",
+  color:'white',
+  alignContent:'center',
+  justifyContent:'center',
+  marginLeft:100  
+  },
+  copy_button_style:{
+    width:250,
+    height:50,
+    backgroundColor:"skyblue",
+    color:'white',
+    alignContent:'center',
+    justifyContent:'center',
+    marginLeft:100 ,
+    marginTop:30 
+    }
+}
 function App() {
+  const [token,setToken] = useState("");
+  const [param,setParams] = useState([]);
 
   var pubKey;
   function encryptMsg() {
 
-    var jsondata = {
-      mobile: "8281790760",
-      firstName: "test",
-      lastName: "testing",
-      loc: "",
-      email: "a8@gmail.c",
-      apikey: "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa",
-      apiSecret: "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
-      lang: "en"
-    }
-
-    var myjson = {
-      "firstName": "Jibi",
-      "lastName": "",
-      "mobile": "9960050008",
-      "email": "",
-      "lang": "en",
-      "loc": "Maharashtra",
-      "apiSecret": "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
-      "apikey": "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa"
-    }
-
-
-  var about = {
-  firstName: "Jibi",
-  lastName: "",
-  mobile: "9946347090",
-  email: "",
-  lang: "en",
-  loc: "Maharashtra",
-  apiSecret: "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
-  apikey: "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa"
-}
-
-var dataaswin = {
-  "firstName": "Varun",
-  "lastName": "",
-  "mobile": "8281790760",
-  "email": "",
-  "lang": "en",
-  "loc": "Maharashtra",
-  "apiSecret": "eLfaH5gsna73_uD9Ulf3czMzbt5khR49qVqciKQULD6OciyI9F",
-  "apikey": "7bEHegXl1qCdRmTvr4EdvTcznuuSJmsgEArLoP_IoOGAxOkV72"
-};
-
- var integrationtestdata = {
-  firstName: "Jibi",
-  lastName: "",
-  mobile: "9946347090",
-  email: "",
-  lang: "en",
-  loc: "Maharashtra",
-  apiSecret: "eLfaH5gsna73_uD9Ulf3czMzbt5khR49qVqciKQULD6OciyI9F",
-  apikey: "7bEHegXl1qCdRmTvr4EdvTcznuuSJmsgEArLoP_IoOGAxOkV72"
-}
-
-// 9971892387it is not wokring with this no.
-var production = {
-  firstName: "test",
-  lastName: "vale",
-  mobile: "9971892382",
-  email: "testpr@g.com",
-  lang: "en",
-  loc: "city",
-  apiSecret: "btaok8_ALMOgOBgOiENDguEVJMd5R77hLNcupwZ9hTQBMhFgrL",
-  apikey: "SQ15fNUc0dzaK1JOx1SZabwnUwOpk12ARMh9GNIL1FUOmLOanM"
-}
-
-      var safalta = {
-          firstName: "Varun",
-          lastName: "",
-          mobile: "9119243233",
-          lang: "en",
-          loc: "Maharashtra",
-          apiSecret: "jFc_OwRCRq3nRhH9CsE_o2OegaOQlabgEFgKwoIwhn96fDAfXY",
-          apikey: "HQogEQpbU4eyXnNwIwhg8sb7NjOMV8VvYCVDgebUJf2PMLMKVK"
-      }
-
-      var vineetSharedJson = {
-
-     "firstName":"राजकुमारी",
-
-          "lastName":"",
-          "mobile":"9119243233",
-          "email":"","lang":"hi","loc":"सतवरिया","apikey":"SQ15fNUc0dzaK1JOx1SZabwnUwOpk12ARMh9GNIL1FUOmLOanM","apiSecret":"btaok8_ALMOgOBgOiENDguEVJMd5R77hLNcupwZ9hTQBMhFgrL"}
-var aswin = {"mobile":"+919946347097","firstName":"test","lastName":"testing","loc":"mycity","email":"a8@gmail.c","apikey":"AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa","apiSecret":"eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1","lang":"en"}
-
-      var champion = {
-        "firstName":"Lak",
-        "loc":"AA",
-        "apikey":"7V9aWBnuZBEh7WF0cjbJRNDY5S9_rnBQlJD24KbheHxNCKfjEE",
-        "apiSecret":"N4XsDHt9QJt1DchHoyrjhOGsj9LFD0jQmg4tesE8L_dPGoXv2H"
-      }
-    const textEncrypt = JSON.stringify(champion);
+    const value = request.fm_production
+    const textEncrypt = JSON.stringify(value);
+    setParams(textEncrypt);
     var encrypt = new JsEncrypt();
     encrypt.setPublicKey(pubKey);
 
     const token = encrypt.encrypt(textEncrypt);
-    // axios , 
-
-    // {
-    //     "status": "SUCCESS",
-    //     "fieldErrorMessageMap": {},
-    //     "redirectionUrl": "https://deploy-preview-1222--sheroes.netlify.app/third-party?token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhOEBnbWFpbC5jb21zZGtsa2xrbGtsa2xzZHNkc2RrbHNkZmtsa2xza2xzZGtsZGZza2xzZGtsc2RrbHNkZmtsc2RzZGYiLCJhdWRpZW5jZSI6IndlYiIsImNyZWF0ZWQiOjE2NTY1MDA0NTg2NjIsImV4cCI6MTY4ODAzNjQ1OCwianRpIjoiOTMwNTI4In0.cMuuz9traeK5Py_JnbubUUxhceM_ofes1TnYmSyumyfsKjoMjaXknf5XLcSyhuWPNCXBjKEjhSwl3M30ZO-OHA&nextScreen=feed_blt",
-    //     "screen_name": null,
-    //     "next_token": null,
-    //     "set_order_key": null,
-    //     "server_feed_config_version": null,
-    //     "error_message": null
-    // }
-
-    console.log("check---ypted tenewxdsfsdft", token);
+    console.log( token);
+    setToken(token);
 
   }
 
@@ -132,9 +54,18 @@ var aswin = {"mobile":"+919946347097","firstName":"test","lastName":"testing","l
       });
 
   })
+
   return (
-    <div className="App">
-      <button onClick={encryptMsg}>Button</button>
+    <div className="App" style={{padding:'25px',margin:'auto',width:'50%',display:'flex', flexDirection:'column' ,justifyContent:'center',alignContent:'center'}}>
+      <p style={{paddingLeft:40}}> Generated Token </p>
+      <textarea type="text" value={token} style={{width:"500px",height:'300px',margin:30}} />
+      <button onClick={encryptMsg} style={styles.button_style}>Generate ThridParty Token </button>
+      <button onClick={() => {
+         if(!token){
+          return;
+         }
+        navigator.clipboard.writeText(token); alert("copied to clipboard")}}
+              style={styles.copy_button_style}>Copy Token </button>
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,130 @@
+var jsondata = {
+    mobile: "8281790760",
+    firstName: "test",
+    lastName: "testing",
+    loc: "",
+    email: "a8@gmail.c",
+    apikey: "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa",
+    apiSecret: "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
+    lang: "en"
+}
+
+var myjson = {
+    "firstName": "Jibi",
+    "lastName": "",
+    "mobile": "9960050008",
+    "email": "",
+    "lang": "en",
+    "loc": "Maharashtra",
+    "apiSecret": "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
+    "apikey": "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa"
+}
+
+
+var about = {
+    firstName: "Jibi",
+    lastName: "",
+    mobile: "9946347090",
+    email: "",
+    lang: "en",
+    loc: "Maharashtra",
+    apiSecret: "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
+    apikey: "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa"
+}
+
+var dataaswin = {
+    "firstName": "Varun",
+    "lastName": "",
+    "mobile": "8281790760",
+    "email": "",
+    "lang": "en",
+    "loc": "Maharashtra",
+    "apiSecret": "eLfaH5gsna73_uD9Ulf3czMzbt5khR49qVqciKQULD6OciyI9F",
+    "apikey": "7bEHegXl1qCdRmTvr4EdvTcznuuSJmsgEArLoP_IoOGAxOkV72"
+};
+
+var integrationtestdata = {
+    firstName: "Jibi",
+    lastName: "",
+    mobile: "9946347090",
+    email: "",
+    lang: "en",
+    loc: "Maharashtra",
+    apiSecret: "eLfaH5gsna73_uD9Ulf3czMzbt5khR49qVqciKQULD6OciyI9F",
+    apikey: "7bEHegXl1qCdRmTvr4EdvTcznuuSJmsgEArLoP_IoOGAxOkV72"
+}
+
+// 9971892387it is not wokring with this no.
+var fm_production = {
+    firstName: "test",
+    lastName: "vale",
+    mobile: "8690224072",
+    email: "testpr@g.com",
+    lang: "en",
+    loc: "city",
+    apiSecret: "btaok8_ALMOgOBgOiENDguEVJMd5R77hLNcupwZ9hTQBMhFgrL",
+    apikey: "SQ15fNUc0dzaK1JOx1SZabwnUwOpk12ARMh9GNIL1FUOmLOanM"
+}
+
+var safalta = {
+    firstName: "Varun",
+    lastName: "",
+    mobile: "9119243233",
+    lang: "en",
+    loc: "Maharashtra",
+    apiSecret: "jFc_OwRCRq3nRhH9CsE_o2OegaOQlabgEFgKwoIwhn96fDAfXY",
+    apikey: "HQogEQpbU4eyXnNwIwhg8sb7NjOMV8VvYCVDgebUJf2PMLMKVK"
+}
+
+var vineetSharedJson = {
+
+    "firstName": "राजकुमारी",
+
+    "lastName": "",
+    "mobile": "9119243233",
+    "email": "", "lang": "hi", "loc": "सतवरिया", "apikey": "SQ15fNUc0dzaK1JOx1SZabwnUwOpk12ARMh9GNIL1FUOmLOanM", "apiSecret": "btaok8_ALMOgOBgOiENDguEVJMd5R77hLNcupwZ9hTQBMhFgrL"
+}
+var aswin = { "mobile": "+919946347097", "firstName": "test", "lastName": "testing", "loc": "mycity", "email": "a8@gmail.c", "apikey": "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa", "apiSecret": "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1", "lang": "en" }
+
+var champion = {
+    "firstName": "Lak",
+    "loc": "AA",
+    "apikey": "7V9aWBnuZBEh7WF0cjbJRNDY5S9_rnBQlJD24KbheHxNCKfjEE",
+    "apiSecret": "N4XsDHt9QJt1DchHoyrjhOGsj9LFD0jQmg4tesE8L_dPGoXv2H"
+}
+
+
+    
+ export const request= {fm_production,aswin,safalta,dataaswin,champion};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         // axios , 
+
+    // {
+    //     "status": "SUCCESS",
+    //     "fieldErrorMessageMap": {},
+    //     "redirectionUrl": "https://deploy-preview-1222--sheroes.netlify.app/third-party?token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhOEBnbWFpbC5jb21zZGtsa2xrbGtsa2xzZHNkc2RrbHNkZmtsa2xza2xzZGtsZGZza2xzZGtsc2RrbHNkZmtsc2RzZGYiLCJhdWRpZW5jZSI6IndlYiIsImNyZWF0ZWQiOjE2NTY1MDA0NTg2NjIsImV4cCI6MTY4ODAzNjQ1OCwianRpIjoiOTMwNTI4In0.cMuuz9traeK5Py_JnbubUUxhceM_ofes1TnYmSyumyfsKjoMjaXknf5XLcSyhuWPNCXBjKEjhSwl3M30ZO-OHA&nextScreen=feed_blt",
+    //     "screen_name": null,
+    //     "next_token": null,
+    //     "set_order_key": null,
+    //     "server_feed_config_version": null,
+    //     "error_message": null
+    // }
