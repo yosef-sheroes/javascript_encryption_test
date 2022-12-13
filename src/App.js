ff416ece3e30151f43891ad5e0415e562d90293c
Yosef <yosef@sheroes.in>
2022-12-13 10:57:32 +0300

    third party javascirpt client library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import JsEncrypt from 'jsencrypt';
+import raw from '../src/hybrid-public.pem';
+import publickey from './thirdpartypublic'
+import { useEffect } from 'react';
 
 function App() {
+
+  var pubKey;
+  function encryptMsg() {
+
+    var jsondata = {
+      mobile: "8281790760",
+      firstName: "test",
+      lastName: "testing",
+      loc: "",
+      email: "a8@gmail.c",
+      apikey: "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa",
+      apiSecret: "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
+      lang: "en"
+    }
+
+    var myjson = {
+      "firstName": "Jibi",
+      "lastName": "",
+      "mobile": "9960050008",
+      "email": "",
+      "lang": "en",
+      "loc": "Maharashtra",
+      "apiSecret": "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
+      "apikey": "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa"
+    }
+
+
+  var about = {
+  firstName: "Jibi",
+  lastName: "",
+  mobile: "9946347090",
+  email: "",
+  lang: "en",
+  loc: "Maharashtra",
+  apiSecret: "eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1",
+  apikey: "AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa"
+}
+
+var dataaswin = {
+  "firstName": "Varun",
+  "lastName": "",
+  "mobile": "8281790760",
+  "email": "",
+  "lang": "en",
+  "loc": "Maharashtra",
+  "apiSecret": "eLfaH5gsna73_uD9Ulf3czMzbt5khR49qVqciKQULD6OciyI9F",
+  "apikey": "7bEHegXl1qCdRmTvr4EdvTcznuuSJmsgEArLoP_IoOGAxOkV72"
+};
+
+ var integrationtestdata = {
+  firstName: "Jibi",
+  lastName: "",
+  mobile: "9946347090",
+  email: "",
+  lang: "en",
+  loc: "Maharashtra",
+  apiSecret: "eLfaH5gsna73_uD9Ulf3czMzbt5khR49qVqciKQULD6OciyI9F",
+  apikey: "7bEHegXl1qCdRmTvr4EdvTcznuuSJmsgEArLoP_IoOGAxOkV72"
+}
+
+// 9971892387it is not wokring with this no.
+var production = {
+  firstName: "test",
+  lastName: "vale",
+  mobile: "9971892382",
+  email: "testpr@g.com",
+  lang: "en",
+  loc: "city",
+  apiSecret: "btaok8_ALMOgOBgOiENDguEVJMd5R77hLNcupwZ9hTQBMhFgrL",
+  apikey: "SQ15fNUc0dzaK1JOx1SZabwnUwOpk12ARMh9GNIL1FUOmLOanM"
+}
+
+      var safalta = {
+          firstName: "Varun",
+          lastName: "",
+          mobile: "9119243233",
+          lang: "en",
+          loc: "Maharashtra",
+          apiSecret: "jFc_OwRCRq3nRhH9CsE_o2OegaOQlabgEFgKwoIwhn96fDAfXY",
+          apikey: "HQogEQpbU4eyXnNwIwhg8sb7NjOMV8VvYCVDgebUJf2PMLMKVK"
+      }
+
+      var vineetSharedJson = {
+
+     "firstName":"राजकुमारी",
+
+          "lastName":"",
+          "mobile":"9119243233",
+          "email":"","lang":"hi","loc":"सतवरिया","apikey":"SQ15fNUc0dzaK1JOx1SZabwnUwOpk12ARMh9GNIL1FUOmLOanM","apiSecret":"btaok8_ALMOgOBgOiENDguEVJMd5R77hLNcupwZ9hTQBMhFgrL"}
+var aswin = {"mobile":"+919946347097","firstName":"test","lastName":"testing","loc":"mycity","email":"a8@gmail.c","apikey":"AUTOkJnEMDrZciwOQepfoB7Sf62BTxnUnaJy2fUacW0Zo3KkRa","apiSecret":"eZcp0drQJBRfuLJ5GnKdYW3fFA3f5STXFXxewEcae0f2Qjisd1","lang":"en"}
+
+      var champion = {
+        "firstName":"Lak",
+        "loc":"AA",
+        "apikey":"7V9aWBnuZBEh7WF0cjbJRNDY5S9_rnBQlJD24KbheHxNCKfjEE",
+        "apiSecret":"N4XsDHt9QJt1DchHoyrjhOGsj9LFD0jQmg4tesE8L_dPGoXv2H"
+      }
+    const textEncrypt = JSON.stringify(champion);
+    var encrypt = new JsEncrypt();
+    encrypt.setPublicKey(pubKey);
+
+    const token = encrypt.encrypt(textEncrypt);
+    // axios , 
+
+    // {
+    //     "status": "SUCCESS",
+    //     "fieldErrorMessageMap": {},
+    //     "redirectionUrl": "https://deploy-preview-1222--sheroes.netlify.app/third-party?token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhOEBnbWFpbC5jb21zZGtsa2xrbGtsa2xzZHNkc2RrbHNkZmtsa2xza2xzZGtsZGZza2xzZGtsc2RrbHNkZmtsc2RzZGYiLCJhdWRpZW5jZSI6IndlYiIsImNyZWF0ZWQiOjE2NTY1MDA0NTg2NjIsImV4cCI6MTY4ODAzNjQ1OCwianRpIjoiOTMwNTI4In0.cMuuz9traeK5Py_JnbubUUxhceM_ofes1TnYmSyumyfsKjoMjaXknf5XLcSyhuWPNCXBjKEjhSwl3M30ZO-OHA&nextScreen=feed_blt",
+    //     "screen_name": null,
+    //     "next_token": null,
+    //     "set_order_key": null,
+    //     "server_feed_config_version": null,
+    //     "error_message": null
+    // }
+
+    console.log("check---ypted tenewxdsfsdft", token);
+
+  }
+
+  useEffect(() => {
+
+    fetch(publickey)
+      .then(r => r.text())
+      .then(content => {
+        pubKey = content;
+      });
+
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={encryptMsg}>Button</button>
     </div>
-  );
+  )
 }
 
 export default App;
